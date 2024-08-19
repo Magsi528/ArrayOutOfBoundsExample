1b186e4d76966c05041994b9fb31aff42536cba7
Dawood Magsi <dawoodmagsi0@gmail.com>
2024-08-19 10:25:01 +0500

    !commit

---
+++ src/ArrayOutOfBoundsExample.java
@@ -0,0 +1,12 @@
+public class ArrayOutOfBoundsExample {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+
+        try {
+            System.out.println(numbers[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index is out of bounds. Please check the index.");
+            System.out.println("Error: " + e);
+        }
+    }
+}
