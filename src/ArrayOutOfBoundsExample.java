745c41358733a3481e88505ef47272074277e376
Dawood Magsi <dawoodmagsi0@gmail.com>
2024-08-19 10:28:48 +0500

    Update ArrayOutOfBoundsExample.java

--- src/ArrayOutOfBoundsExample.java
+++ src/ArrayOutOfBoundsExample.java
@@ -1,6 +1,6 @@
 public class ArrayOutOfBoundsExample {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3};
+        int[] numbers = {1, 2, 3}
 
         try {
             System.out.println(numbers[5]);
